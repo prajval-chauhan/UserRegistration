96963d9292ec909e1908a21dd6975b8623f302b7
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-07 20:09:36 +0530

    First Name Validation

--- Program.cs
+++ Program.cs
@@ -8,9 +8,21 @@ namespace UserRegistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the new user registration program");
+            firstNameCheck();
 
             
         }
+        static void firstNameCheck()
+        {
+            string name = "^[A-Z]{1}[a-zA-Z0-9]{2,15}$";
+            Regex reg = new Regex(name);
+            Console.WriteLine("Enter your First Name: ");
+            string firstName = Console.ReadLine();
+            if (reg.IsMatch(firstName))
+                Console.WriteLine("Valid");
+            else
+                Console.WriteLine( "Invalid"); 
 
+        }
     }
 }
