7384abea7809386b9918a302be76638fcbf087fa
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-13 02:29:09 +0530

    Refactored the code

---
+++ DataValidation.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistration
+{
+    public class DataValidation
+    {
+        static string name_regex = "^[A-Z]{1}[a-zA-Z0-9]{2,}$";
+        static string eMail_regex = "[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
+        static string mobileNumber_regex = "^[\\d]{2}\\s[\\d]{10}$";
+        static string password_regex = "^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[@.,;$%^&#{}+_]{1}).{8,}$";
+
+        public string firstName;
+        public string lastName;
+        public string email;
+        public string mobileNo;
+        public string password;
+
+        public bool NameCheck(string name)
+        {
+            Regex reg = new Regex(name_regex);
+            if (reg.IsMatch(name))
+            {
+                Console.WriteLine("Valid\n");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+                Console.ReadKey();
+                Console.Clear();
+                return false;
+            }
+
+        }
+        public bool mobileNumberValidation(string mobileNumber)
+        {
+            Regex re = new Regex(mobileNumber_regex);
+            if (re.IsMatch(mobileNumber))
+            {
+                Console.WriteLine("Valid\n");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+                Console.ReadKey();
+                Console.Clear();
+                return false;
+            }
+        }
+        public bool emailValidation(string email)
+        {
+            Regex re = new Regex(eMail_regex);
+            if (re.IsMatch(email))
+            {
+                Console.WriteLine("Valid\n");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+                Console.ReadKey();
+                Console.Clear();
+                return false;
+            }
+        }
+        public bool passwordValidation(string password)
+        {
+            Regex re = new Regex(password_regex);
+            if (re.IsMatch(password))
+            {
+                Console.WriteLine("Valid\n");
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+                Console.ReadKey();
+                Console.Clear();
+                return false;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -3,82 +3,36 @@ using System.Text.RegularExpressions;
 
 namespace UserRegistration
 {
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the new user registration program");
-            firstNameCheck();
-            lastNameCheck();
-            emailValidation();
-            mobileNumberValidation();
-            passwordValidation();
+            DataValidation call = new DataValidation();
+            Console.WriteLine("***Welcome to the new user registration program***");
+            do
+            {
+                Console.WriteLine("Enter the first name: ");
+                call.firstName = Console.ReadLine();
+            } while (call.NameCheck(call.firstName) == false);
 
-            
-        }
-        static void firstNameCheck()
-        {
-            string name = "^[A-Z]{1}[a-zA-Z0-9]{2,15}$";
-            Regex reg = new Regex(name);
-            Console.WriteLine("Enter your First Name: ");
-            string firstName = Console.ReadLine();
-            if (reg.IsMatch(firstName))
-                Console.WriteLine("Valid");
-            else
-                Console.WriteLine( "Invalid"); 
+            do
+            {
+                Console.WriteLine("Enter the Last name: ");
+                call.lastName = Console.ReadLine();
+            } while (call.NameCheck(call.lastName) == false);
 
-        }
+            do
+            {
+                Console.WriteLine("Enter your mobile number: ");
+                call.mobileNo = Console.ReadLine();
+            } while (call.mobileNumberValidation(call.mobileNo) == false);
 
-        static void lastNameCheck()
-        {
-            string name = "^[A-Z]{1}[a-zA-Z0-9]{2,15}$";
-            Regex reg = new Regex(name);
-            Console.WriteLine("Enter your last Name: ");
-            string lastName = Console.ReadLine();
-            if (reg.IsMatch(lastName))
-                Console.WriteLine("Valid");
-            else
-                Console.WriteLine("Invalid");
-
-        }
-
-        public static void emailValidation()
-        {
-            string eMail = "[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
-            Console.WriteLine("Enter the email ID you want to validate: ");
-            string input = Console.ReadLine();
-
-            Regex re = new Regex(eMail);
-            if (re.IsMatch(input))
-                Console.WriteLine("Email ID is valid");
-            else
-                Console.WriteLine("Email ID is not valid");
-        }
-
-        public static void mobileNumberValidation()
-        {
-            string mobileNumber = "^[\\d]{2}\\s[\\d]{10}$";
-            Console.WriteLine("Enter the mobile number : ");
-            string input = Console.ReadLine();
-
-            Regex re = new Regex(mobileNumber);
-            if (re.IsMatch(input))
-                Console.WriteLine("Mobile Number is valid");
-            else
-                Console.WriteLine("Mobile Number is not valid");
-        }
-
-        public static void passwordValidation()
-        {
-            string password = "^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[@.,;$%^&#{}+_]{1}).{8,}$";
-            Console.WriteLine("Enter the password : ");
-            string input = Console.ReadLine();
-
-            Regex re = new Regex(password);
-            if (re.IsMatch(input))
-                Console.WriteLine("Password is valid");
-            else
-                Console.WriteLine("Password is not valid");
+            do
+            {
+                Console.WriteLine("Enter the email address: ");
+                call.email = Console.ReadLine();
+            } while (call.emailValidation(call.email) == false);
         }
     }
 }
