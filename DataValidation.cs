10c0fa765ec8720e8e68ba096e61be409846c4b3
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-14 01:34:25 +0530

    All the given email samples were cleared

--- DataValidation.cs
+++ DataValidation.cs
@@ -7,17 +7,23 @@ namespace UserRegistration
 {
     public class DataValidation
     {
-        static string name_regex = "^[A-Z]{1}[a-zA-Z0-9]{2,}$";
+        //Regex expression according to the conditions
+        static string name_regex = "^[A-Z]{1}[a-zA-Z]{2,}$";
         static string eMail_regex = "[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
         static string mobileNumber_regex = "^[\\d]{2}\\s[\\d]{10}$";
-        static string password_regex = "^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[@.,;$%^&#{}+_]{1}).{8,}$";
-
+        static string password_regex = "^(?=.*?[A-Z]{1})(?=.*?[0-9])(?=.*?[@.,;$%^&#{}+_]{1}).{8,}$";
+        //parameters that are to be validated
         public string firstName;
         public string lastName;
         public string email;
         public string mobileNo;
         public string password;
 
+        /// <summary>
+        /// Validates the First name and last name of the User
+        /// </summary>
+        /// <param name="name">The name.</param>
+        /// <returns></returns>
         public bool NameCheck(string name)
         {
             Regex reg = new Regex(name_regex);
@@ -35,6 +41,11 @@ namespace UserRegistration
             }
 
         }
+        /// <summary>
+        /// Validates the Mobile Number
+        /// </summary>
+        /// <param name="mobileNumber">The mobile number.</param>
+        /// <returns></returns>
         public bool mobileNumberValidation(string mobileNumber)
         {
             Regex re = new Regex(mobileNumber_regex);
@@ -51,6 +62,11 @@ namespace UserRegistration
                 return false;
             }
         }
+        /// <summary>
+        /// Validates the email Address
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <returns></returns>
         public bool emailValidation(string email)
         {
             Regex re = new Regex(eMail_regex);
@@ -67,6 +83,11 @@ namespace UserRegistration
                 return false;
             }
         }
+        /// <summary>
+        /// validates the password
+        /// </summary>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
         public bool passwordValidation(string password)
         {
             Regex re = new Regex(password_regex);
